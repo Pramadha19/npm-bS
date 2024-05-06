a2fc0295a9a566dd47a279113b9e98e1cd0fc15c
Pramadha Yaramakala <pramadhayaramakala@Pramadhas-MacBook-Pro.local>
2024-05-06 21:07:45 +0530

    npm

---
+++ test.js
@@ -0,0 +1,12 @@
+const {
+    binarySearch,
+    
+  } = require('./index'); 
+  // Test data
+  const array = [1, 2, 3, 4, 5];
+  const target = 3;
+  
+  //  binarySearch
+  console.log('Binary Search:', binarySearch(array, target)); // Should return 2
+  
+  
